492e5c26d3833fa2cef2355dae14570dbd7b72a8
unknown <joseotaviocbueno@gmail.com>
2022-09-12 22:09:40 -0300

    .

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  
+	constructor ( props ) {
+		super(props)
+		this.state = {
+			hora: "00:00:00"
+		};
+	}
+
+	componentDidMount () {
+		setInterval( () => {
+			this.setState({ hora: new Date().toLocaleTimeString() });
+		}, 1000 );
+	}
+
+	// componentDidUpdate () {
+	// 	console.log("Atualizou!!!")
+	// }
+
+	render () {
+ return (
+   <div>
+     <h1> Meu projeto { this.state.hora } </h1>
+   </div>
+  )
+ }
+}
