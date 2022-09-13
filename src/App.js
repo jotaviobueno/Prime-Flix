df46302db445d9aa58a717a310220c8139433b6a
unknown <joseotaviocbueno@gmail.com>
2022-09-13 14:56:17 -0300

    Feed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from "react";
-import User from './components/User/User.js'
+import React, { Component } from  'react';
 
-export default class App extends Component {
-  
-	render () {
-		return (
-			<div>
-				<User name="Visitante" />
-			</div>
-		);
-	}
+import Feed from "./components/feed/Feed.js"
+
+export default class User extends Component {
+    
+    constructor ( props ) {
+        super(props);
+        this.state = {
+            feed: [
+                { id: 23, username: "Matheus", like: 10, comments: 5 },
+                { id: 321, username: "Pedro", like: 1230, comments: 123 },
+                { id: 1, username: "Otavio", like: 1, comments: 9921 },
+                
+            ]
+        };
+
+    }
+    
+    render () {
+        return (
+            <div>
+
+            { this.state.feed.map( ( post ) => {
+                    return (
+                        <Feed id = { post.id } username = { post.username } like = { post.like } comments = { post.comments }    />
+                    )
+                })
+            } 
+            </div>
+        );
+    }
 }
