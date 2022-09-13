73383cc31613fc40deefbe8fbb9b690f5aaf165a
unknown <joseotaviocbueno@gmail.com>
2022-09-13 16:36:26 -0300

    new

--- src/App.js
+++ src/App.js
@@ -1,57 +1,53 @@
 import React, { Component } from  'react';
 
 export default class User extends Component {
-    
+
     constructor ( props ) {
         super(props);
         this.state = {
-			name: "",
-            email: "",
-            password: "",
-            error: ""
-
+            form: {
+                name: "",
+                email: "",
+                password: "",
+                sexo: ""
+            }
         };
 
-        this.send = this.send.bind( this );
-    
+        this.changeData = this.changeData.bind( this );
+
     }
 
-    send ( event ) {
-        const { name, email, password } = this.state;
+    changeData ( event ) {
+        let form = this.state.form;
 
-        if ( name !== "" && email !== "" && password !== "" )
-            alert( ` 
-            Nome: ${name}
-            Email: ${email}
-            password: ${password}
-            ` );
-        
-        this.setState( { error: "Campos Invalidos" } );
-
-        event.preventDefault();
-        
-    }
-  
+        form[event.target.name] = event.target.value;
+        this.setState({ form: form })
+    } 
 
     render () {
         return (
             <div>
-                <h1> Novo usuario </h1>
-                { this.state.error && <p> { this.state.error } </p> }
-                    <form onSubmit={ this.send } >
-                        <label> Username: </label>
-                        <input type= "text" value= { this.state.name } onChange= { ( event ) => this.setState({ name: event.target.value }) } /> <br/>
+				<h2> Login </h2>
 
-                        <label> Email: </label>
-                        <input type= "email" value= { this.state.email } onChange= { ( event ) => this.setState({ email: event.target.value }) } /> <br/>
+                Nome:
+                    <input type= "text" name= "name" value= { this.state.form.name } onChange= { this.changeData } /> <br/>
+				Email:
+					<input type= "email" name= "email" value= { this.state.form.email  } onChange= { this.changeData } /> <br/>
+				Password:
+					<input type= "password" name= "password" value= { this.state.form.password } onChange= { this.changeData } /> <br/>
+				Sexo:
+                    <select name= "sexo" value= { this.state.sexo } onChange= { this.changeData } >
+                        <option value= "masculino" > Masculino </option>    
+                        <option value= "feminino"  > Feminino </option>   
+                        <option value= "n/a" > Prefiro não responder </option>    
+                    </select>	
 
-                        <label> Password: </label>
-                        <input type= "password" value= { this.state.password } onChange= { ( event ) => this.setState({ password: event.target.value }) } /> <br/>
-
-
-                        <button type= "submit" > Send </button>
-                    </form>
+                <div>
+                    <h3> { this.state.form.email } </h3>
+                    <h3> { this.state.form.password } </h3>
+                    <h3> { this.state.form.sexo } </h3>
+                </div>
             </div>
-        )
+        );
     }
 }
